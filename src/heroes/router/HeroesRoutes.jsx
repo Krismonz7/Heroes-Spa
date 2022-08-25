880b1a5f4bdb6555f315e0a5e2e7ca2f12608edf
HANS <marcel.hans.monzon.p@gmail.com>
2022-08-25 09:41:35 -0500

    First commit

---
+++ src/heroes/router/HeroesRoutes.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Login } from '../../auth'
+import { Navbar } from '../../ui'
+import { Search } from '../pages/Search'
+import { HeroPage } from '../pages/HeroPage'
+import { DcPage } from '../pages/DcPage'
+import { MarvelPage } from '../pages/MarvelPage'
+
+
+export const HeroesRoutes = () => {
+    return (
+    <>
+    <Navbar/>
+    <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DcPage />} />
+        <Route path='search' element={<Search/>}></Route>
+        <Route path='hero' element={<HeroPage/>}></Route>
+        {/*Search, Hero by id */}
+        <Route path='login' element={<Login/>}></Route>
+        <Route path='/' element={<Navigate to={'marvel'}/>}></Route>
+    </Routes>
+
+    </>
+    )
+}
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { Login } from '../auth'
+import { HeroesRoutes } from '../heroes'
+import { Navbar } from '../ui'
+
+
+export const AppRouter = () => {
+return (
+    <div>
+
+        <Navbar/>
+
+        <div className="container">
+        <Routes>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/*' element={<HeroesRoutes/>}></Route>
+        </Routes>
+        </div>
+    </div>
+)
+}
